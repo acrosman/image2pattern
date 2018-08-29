ec9bb6755c8a06d6d976123a586c64135dc4607b
Aaron Crosman <acrosman@gmail.com>
2018-08-29 00:36:17 -0400

    Add function to find closest thread

--- src/threadColors.js
+++ src/threadColors.js
@@ -25,9 +25,9 @@ function rgb2Hex(rgb) {
 // found here: https://www.easyrgb.com/en/math.php.
 
 function lab2Rgb(lab) {
-  let y = (lab[0] + 16) / 116;
-  let x = lab[1] / 500 + y;
-  let z = y - lab[2] / 200;
+  let y = (lab.L + 16) / 116;
+  let x = lab.a / 500 + y;
+  let z = y - lab.b / 200;
   let r; let g; let b;
 
   x = 0.95047 * ((x * x * x > 0.008856) ? x * x * x : (x - 16 / 116) / 7.787);
@@ -38,33 +38,34 @@ function lab2Rgb(lab) {
   g = x * -0.9689 + y * 1.8758 + z * 0.0415;
   b = x * 0.0557 + y * -0.2040 + z * 1.0570;
 
-  r = (r > 0.0031308) ? (1.055 * Math.pow(r, 1 / 2.4) - 0.055) : 12.92 * r;
-  g = (g > 0.0031308) ? (1.055 * Math.pow(g, 1 / 2.4) - 0.055) : 12.92 * g;
-  b = (b > 0.0031308) ? (1.055 * Math.pow(b, 1 / 2.4) - 0.055) : 12.92 * b;
+  r = (r > 0.0031308) ? (1.055 * (r ** (1 / 2.4)) - 0.055) : 12.92 * r;
+  g = (g > 0.0031308) ? (1.055 * (g ** (1 / 2.4)) - 0.055) : 12.92 * g;
+  b = (b > 0.0031308) ? (1.055 * (b ** (1 / 2.4)) - 0.055) : 12.92 * b;
 
-  return [Math.max(0, Math.min(1, r)) * 255,
-    Math.max(0, Math.min(1, g)) * 255,
-    Math.max(0, Math.min(1, b)) * 255,
-  ];
+  return {
+    r: Math.max(0, Math.min(1, r)) * 255,
+    g: Math.max(0, Math.min(1, g)) * 255,
+    b: Math.max(0, Math.min(1, b)) * 255,
+  };
 }
 
 function rgb2Lab(rgb) {
-  let r = rgb[0] / 255;
-  let g = rgb[1] / 255;
-  let b = rgb[2] / 255;
+  let r = rgb.r / 255;
+  let g = rgb.g / 255;
+  let b = rgb.b / 255;
   let x; let y; let z;
 
-  r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
-  g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
-  b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+  r = (r > 0.04045) ? (((r + 0.055) / 1.055) ** 2.4) : r / 12.92;
+  g = (g > 0.04045) ? (((g + 0.055) / 1.055) ** 2.4) : g / 12.92;
+  b = (b > 0.04045) ? (((b + 0.055) / 1.055) ** 2.4) : b / 12.92;
 
   x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
   y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
   z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
 
-  x = (x > 0.008856) ? Math.pow(x, 1 / 3) : (7.787 * x) + 16 / 116;
-  y = (y > 0.008856) ? Math.pow(y, 1 / 3) : (7.787 * y) + 16 / 116;
-  z = (z > 0.008856) ? Math.pow(z, 1 / 3) : (7.787 * z) + 16 / 116;
+  x = (x > 0.008856) ? x ** (1 / 3) : (7.787 * x) + 16 / 116;
+  y = (y > 0.008856) ? y ** (1 / 3) : (7.787 * y) + 16 / 116;
+  z = (z > 0.008856) ? z ** (1 / 3) : (7.787 * z) + 16 / 116;
 
   return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)];
 }
@@ -87,10 +88,29 @@ function deltaE(labA, labB) {
   return i < 0 ? 0 : Math.sqrt(i);
 }
 
+// Calculate the DeltaE between two RGB objects.
 function colorDistance(rgb1, rgb2) {
   const lab1 = rgb2Lab(rgb1);
   const lab2 = rgb2Lab(rgb2);
   return deltaE(lab1, lab2);
+}
+
+function closestThreadColor(rgb) {
+  const lab = rgb2Lab(rgb);
+  let closest = {};
+  let distance = 100;
+  let testDist = -1;
+  for (let i = 0; i < DMC.length; i += 1) {
+    testDist = deltaE(lab, DMC[i].lab);
+    if (testDist < distance) {
+      distance = testDist;
+      closest = DMC[i];
+    }
+    if (distance < 1) {
+      break;
+    }
+  }
+  return closest;
 }
 
 exports.rgb2Hex = rgb2Hex;
@@ -100,3 +120,4 @@ exports.lab2Rgb = lab2Rgb;
 exports.rbg2Lab = rgb2Lab;
 exports.detlaE = deltaE;
 exports.colorDistance = colorDistance;
+exports.closestThreadColor = closestThreadColor;
