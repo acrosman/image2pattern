f9a09f509d3b9f52b63fd2286736063a5e910c0a
Aaron Crosman <acrosman@gmail.com>
2018-09-03 22:13:36 -0400

    Refactored to put jQuery code into own file. Added a switch between monochrome and color.

---
+++ renderer/interactions.js
@@ -0,0 +1,62 @@
+/**
+ * jQuery interactions. Events that need main thread processing are in renderer.js.
+ */
+
+function showColorControls(mode) {
+  const vibrant = '.control-set--vibrant';
+  const mono = '.control-set--monochrome';
+
+  if (mode === 'vibrant') {
+    $(vibrant).show();
+    $(mono).hide();
+  } else {
+    $(vibrant).hide();
+    $(mono).show();
+  }
+}
+
+$('#color-mode-selector').on('change', (event) => {
+  showColorControls($('#color-mode-selector').val());
+});
+showColorControls($('#color-mode-selector').val());
+
+// Basic instantiation of color pickers:
+// TODO: change color picker library, or similar approach to remove jQuery and
+// bootstrap dependencies at some point.
+$('#dark-color-swatch').colorpicker({
+  format: 'hex',
+  color: '#444444',
+  container: true,
+  inline: true,
+});
+
+// Example using an event, to change the color of the .jumbotron background:
+$('#dark-color-swatch').on('colorpickerChange', (event) => {
+  $('#dark-color').val(event.color.toString());
+});
+
+// Basic instantiation of color pickers:
+$('#light-color-swatch').colorpicker({
+  format: 'hex',
+  color: '#FFFF22',
+  container: true,
+  inline: true,
+});
+
+// Example using an event, to change the color of the .jumbotron background:
+$('#light-color-swatch').on('colorpickerChange', (event) => {
+  $('#light-color').val(event.color.toString());
+});
+
+// Basic instantiation of color pickers:
+$('#grid-color-swatch').colorpicker({
+  format: 'hex',
+  color: '#000000',
+  container: true,
+  inline: true,
+});
+
+// Example using an event, to change the color of the .jumbotron background:
+$('#grid-color-swatch').on('colorpickerChange', (event) => {
+  $('#grid-color').val(event.color.toString());
+});
