b6d65186f49088e1e23f1bf19c2dac4772de6849
Cortlan Bainbridge <bainbric@uni.edu>
2018-10-21 12:55:38 -0500

    This should be a private method to this file, its not used anywhere else

--- src/colorUtils.js
+++ src/colorUtils.js
@@ -9,22 +9,34 @@ function hexToRgb(hex) {
   } : null;
 }
 
-function component2Hex(c) {
-  const hex = c.toString(16);
-  return hex.length === 1 ? `0${hex}` : hex;
-}
-
 // Convert an rgb object into a css-style hex value
 function rgb2Hex(rgb) {
+  function component2Hex(c) {
+    const hex = c.toString(16);
+    return hex.length === 1 ? `0${hex}` : hex;
+  }
+
   return `#${component2Hex(rgb.r)}${component2Hex(rgb.g)}${component2Hex(rgb.b)}`;
 }
 
 // Convert a standard CSS style hex color value to the integer value that JIMP
 // expects. Assumes the CSS value is in the form '#[RR][GG][BB]' and adds unless
 // an alpha is provided produces an totally solid color.
-function cssHex2JimpInt(cssValue, alphaChannel = 'FF') {
-  const color = `0x${cssValue.slice(-6)}${alphaChannel}`;
+function cssHex2JimpInt(colorHex, alphaChannel = 'FF') {
+  const color = `0x${colorHex.slice(-6)}${alphaChannel}`;
   return parseInt(color, 16); // Done it two steps to ease debugging.
+}
+
+// Convert int color to css hex without Alpha channel.
+function int2CssHex(intColor) {
+  let hexString = `#${intColor.toString(16).toUpperCase()}`;
+
+  // If the color is too small, to be detected, go to black.
+  if (hexString.length < 9) {
+    hexString = '#00000000';
+  }
+
+  return hexString.slice(0, -2);
 }
 
 // Calculate the DeltaE between two RGB objects.
@@ -52,18 +64,6 @@ function closestColor(labColor, labColorList) {
   return closest;
 }
 
-// Convert int color to css hex without Alpha channel.
-function int2CssHex(intColor) {
-  let hexString = `#${intColor.toString(16).toUpperCase()}`;
-
-  // If the color is too small, to be detected, go to black.
-  if (hexString.length < 9) {
-    hexString = '#00000000';
-  }
-
-  return hexString.slice(0, -2);
-}
-
 // Uses the W3C guideline for a dark color vs light.
 function isDarkColor(rgb) {
   // See https://www.w3.org/TR/AERT/#color-contrast
@@ -72,7 +72,6 @@ function isDarkColor(rgb) {
 
 exports.cssHex2JimpInt = cssHex2JimpInt;
 exports.rgb2Hex = rgb2Hex;
-exports.component2Hex = component2Hex;
 exports.hex2Rgb = hexToRgb;
 exports.colorDistance = colorDistance;
 exports.closestColor = closestColor;
